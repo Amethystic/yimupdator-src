fa1aed603a85a9c4ce507edc05d011f3fee8ab12
Athena <119145022+Amethystic@users.noreply.github.com>
2023-12-12 05:24:17 -0600

    wat i did so far

---
+++ SRC/etc/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using YimUpdater.SRC.etc.Anims;
+
+namespace YimUpdater.SRC.etc
+{
+    internal class Startup
+    {
+        public static void Do()
+        {
+            Console.Title = "Please wait...";
+            Console.WriteLine(@"
+
+         @@@@@@@@@@@@@@@                          
+     @@@@                @@@@@@                   
+  @@@                           @@@               
+ @@    @@                           @@@           
+@@                @                   @@@         
+@@@                                      @@       
+     @@@@@@@@@@@@@@@                      @@      
+            @@           @@@@              @@     
+            @                               @@    
+           @@                                @    
+           @                                 @@   
+          @@                                  @@  
+          @                                    @  
+         @@                                    @  
+         @                                     @@ 
+        @@                               @@@@   @ 
+        @@    @@                    @@@@        @ 
+        @        @@@           @@@@             @@
+        @           @@     @@@@                 @@
+       %@             @@@@@@                    @@
+       @@              @@@@@@@@                 @@
+       @@              @@@@@@@@                 @@
+       @                @@@@                    @@
+       @                                        @@
+      @@                                        @@
+      @@                                        @ 
+      @                                         @ 
+
+");
+            Task.Delay(2000).Wait();
+            Console.Clear();
+            Animation.Do();
+        }
+    }
+}
